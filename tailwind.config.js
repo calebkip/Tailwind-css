1530907cb6ed95f9c15f2368e977f838b26bbee2
calebkip <calebkip.ck@gmail.com>
2022-04-27 19:42:15 +0300

    final page

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors:{
+        primary:'#446A46',
+        primary2:'#82A284',
+        basic:'#E4AC5'
+      }
+      
+    },
+    fontFamily:{
+      body:['Roboto']
+    }
+  },
+  plugins: [],
+}
